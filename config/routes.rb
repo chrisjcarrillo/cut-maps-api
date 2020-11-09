7263b0210e16934f22b835dbc568236d8fc1cb0a
Chris Carrillo <chrisjcarrillo@hotmail.com>
2020-11-09 06:53:52 -0500

    changes

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,7 @@
 Rails.application.routes.draw do
   namespace :api do
     namespace :v1 do
-      resources :uploads do
+      resources :uploads, only: [:index, :show] do
         collection do
           get 'get_folder'
           post 'upload_file'

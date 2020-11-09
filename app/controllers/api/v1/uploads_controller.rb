e2677a623fdd93548c46d4ef281a3fb51384ebe3
Chris Carrillo <chrisjcarrillo@hotmail.com>
2020-11-09 06:45:53 -0500

    init

---
+++ app/controllers/api/v1/uploads_controller.rb
@@ -0,0 +1,31 @@
+require 'boxr'
+class Api::V1::UploadsController < ApplicationController
+    CLIENT = Boxr::Client.new('JlC5mGfAKg7pL43Osw1qpvztHYfzD4q6')
+    PARENT_FOLDER = 124248437317;
+
+    def get_folder
+        @items = CLIENT.folder_items(Boxr::ROOT)
+        @items.each {|i| puts i.name}
+        render json: @items
+    end
+
+    def create_folder
+        name = params[:name]
+        @folder = CLIENT.create_folder(name, PARENT_FOLDER)
+        render json: @folder
+    end
+
+    def upload_image
+        image = params[:image]
+        folder = params[:folder_id]
+        file_name = params[:file_name]
+        @folder = CLIENT.folder_from_id(124247017735, fields: [])
+        @file = CLIENT.upload_file(image, @folder, name: file_name)
+        render json: @file
+    end
+    
+    def upload_pdf
+        @folder = CLIENT.folder_from_id(124248437317, fields: [])
+        @file = CLIENT.upload_file('test.txt', @folder)
+    end
+end
---
+++ config/routes.rb
@@ -0,0 +1,18 @@
+Rails.application.routes.draw do
+  namespace :api do
+    namespace :v1 do
+      resources :uploads do
+        collection do
+          get 'get_folder'
+          post 'upload_file'
+          post 'create_folder'
+          # get 'reports'
+          # get 'detailed_leads'
+          # get 'generate_accelerated_responsivness'
+          # root 'lsa_reports#accounts'
+        end
+      end
+    end
+  end
+  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+end
